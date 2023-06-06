8d7eceef040cad90628c8c97fc19345d6a8bf932
Sleaze <sleazestiks@gmail.com>
2023-06-06 22:13:09 +0200

    Adds DontDestroyOnLoad and SelfDestruct

---
+++ Utilities/DontDestroyOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace TeamSleaze.Utilities
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        public bool Preserve;
+
+        private void Awake()
+        {
+            if (Preserve) DontDestroyOnLoad(this);
+        }
+    }
+}
---
+++ Utilities/SelfDestruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace TeamSleaze.Utilities
+{
+    public class SelfDestruct : MonoBehaviour
+    {
+        public bool ShouldSelfDestruct;
+        public float WaitSeconds;
+
+        private void Awake()
+        {
+            StartCoroutine(Destruct());
+        }
+
+        private IEnumerator Destruct()
+        {
+            yield return Helpers.GetWaitForSeconds(WaitSeconds);
+        }
+    }
+}
