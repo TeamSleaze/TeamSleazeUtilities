b6b069298031df4c843c9dd2b4de3ddb5983b253
Sleaze <sleazestiks@gmail.com>
2023-06-07 17:29:17 +0200

    Implements Timer and AdjustableTimer
    Version 0.3.0

---
+++ Utilities/Timer/AdjustableTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+
+namespace TeamSleaze.Utilities.Timer
+{
+    public class AdjustableTimer : Timer
+    {
+        public float Interval;
+
+        public delegate void AdjustableAmountOfSecondsPassed();
+        public static event AdjustableAmountOfSecondsPassed OnAdjustableAmountOfSecondsPassed;
+
+
+        protected override void Start()
+        {
+            if (Interval == 0) {
+                throw new NoNullAllowedException("Interval can't be null!");
+            }
+            else StartCoroutine(CallEvent(OnAdjustableAmountOfSecondsPassed, Interval));
+        }
+    }
+}
---
+++ Utilities/Timer/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace TeamSleaze.Utilities.Timer
+{
+    public abstract class Timer : MonoBehaviour
+    {
+        public static float currentTime { private set; get; } = 0f;
+        public static int currentTimeInt { private set; get; } = 0;
+
+
+        protected abstract void Start();
+
+        private void Update()
+        {
+            currentTime += Time.deltaTime;
+            currentTimeInt = Mathf.FloorToInt(currentTime);
+        }
+
+        protected IEnumerator CallEvent(Delegate e, float time)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(time);
+                e.DynamicInvoke();
+            }
+        }
+
+    }
+}
