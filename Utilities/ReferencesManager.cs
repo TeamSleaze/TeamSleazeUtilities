14889cd1c354ede9781e4f139f8243d7c24f19cf
Sleaze <sleazestiks@gmail.com>
2023-06-10 18:45:45 +0200

    Adds ReferencesManager
    Version 0.9.0

---
+++ Utilities/ReferencesManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TeamSleaze.Utilities
+{
+    public class ReferencesManager : MonoBehaviour
+    {
+        public static ReferencesManager Instance;
+
+        public GameObject Player;
+
+
+        private void Start()
+        {
+            Instance = this;
+
+            Player = GameObject.FindGameObjectsWithTag("Player")[0];
+        }
+    }
+}
